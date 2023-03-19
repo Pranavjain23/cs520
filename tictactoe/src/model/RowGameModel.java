5341f644394b64c4c41151198c109391bb2fac4e
Pranavjain23 <ipranavjain@gmail.com>
2023-03-19 16:21:20 -0400

    completed Modularity (Open-closed principle)

--- tictactoe/src/model/RowGameModel.java
+++ tictactoe/src/model/RowGameModel.java
@@ -10,27 +10,42 @@ public class RowGameModel
     /**
      * The current player taking their turn
      */
-    public String player = "1";
+    private Player player = Player.PLAYER_1;
     public int movesLeft = 9;
 
     private String finalResult = null;
 
+    public enum Player {
+        PLAYER_1,
+        PLAYER_2
+    }
 
     public RowGameModel() {
-	super();
+        super();
 
-	for (int row = 0; row < 3; row++) {
-	    for (int col = 0; col < 3; col++) {
-		blocksData[row][col] = new RowBlockModel(this);
-	    } // end for col
-	} // end for row
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                blocksData[row][col] = new RowBlockModel(this);
+            } // end for col
+        } // end for row
     }
 
     public String getFinalResult() {
-	return this.finalResult;
+        return this.finalResult;
     }
 
     public void setFinalResult(String finalResult) {
-	this.finalResult = finalResult;
+        this.finalResult = finalResult;
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public void setPlayer(Player player) {
+        if (player != Player.PLAYER_1 && player != Player.PLAYER_2) {
+            throw new IllegalArgumentException("Invalid player: " + player);
+        }
+        this.player = player;
     }
 }
